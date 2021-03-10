f32a9e47cbf4b761d90e1165e8891d5ecc7a3c5b
thonpair <emmanuel@hemer.it>
2021-03-10 18:33:11 +0100

    change css path for build

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import './style.css'
+
 fetch('https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json')
 .then(response => response.json())
 .then(dataEducation => {
